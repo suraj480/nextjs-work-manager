d0fc2bc9c9fdc55d6cc651f29157c0d96a0056d4
suraj480 <surajmaurya991@gmail.com>
2023-10-20 22:56:54 +0530

    get single user

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,5 +1,23 @@
 import { User } from "@/models/user";
 import { NextResponse } from "next/server";
+
+//get single user
+export async function GET(request, { params }) {
+  const { userId } = params;
+  try {
+    const user = await User.findById(userId);
+    return NextResponse.json(user, {
+      message: "Details of selected user !!",
+      success: true,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: "Selected user not found",
+      success: false,
+    });
+  }
+}
+
 //DELETE request function
 export async function DELETE(request, { params }) {
   console.log(params);
