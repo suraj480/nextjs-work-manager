09069c5ff7ca91efd5ad2737938993454686f996
suraj480 <surajmaurya991@gmail.com>
2023-10-26 10:04:51 +0530

    designing home

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
+import ActionSection from "./components/homepage/ActionSection";
+import ContactForm from "./components/homepage/ContactForm";
+import FeatureSection from "./components/homepage/FeatureSection";
+import BannerSection from "./components/homepage/HomeBanner";
+import TestimonialSection from "./components/homepage/TestimonialSection";
+
 export const metadata = {
   title: "Work Manager",
 };
 export default function Home() {
   return (
-    <main>
+    // <main>
       <div>
-        <h1 className="text-5xl">Welcome to work manager</h1>
+   <BannerSection/>
+   <FeatureSection/>
+   <ActionSection/>
+   <TestimonialSection/>
+   <ContactForm/>
+        {/* <h1 className="text-5xl">Welcome to work manager</h1> */}
       </div>
-    </main>
+    // </main>
   );
 }
